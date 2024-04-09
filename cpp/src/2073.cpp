210ee357407433f169510f2427031f5b3e7ae959
Jayden Filippi <jaydenjfilippi@gmail.com>
2024-04-09 20:16:31 +1000

    complete question 2073

---
+++ cpp/src/2073.cpp
@@ -0,0 +1,26 @@
+#include <cstddef>
+#include <vector>
+
+using std::vector;
+
+class Solution
+{
+  public:
+    int
+    timeRequiredToBuy(vector<int>& tickets, int k)
+    {
+        int elapsed_time = 0;
+        for(size_t i = 0; i < k; i++)
+        {
+            elapsed_time += std::min(tickets[k], tickets[i]);
+        }
+
+        elapsed_time += tickets[k];
+
+        for(size_t i = k + 1; i < tickets.size(); i++)
+        {
+            elapsed_time += std::min(tickets[k] - 1, tickets[i]);
+        }
+        return elapsed_time;
+    }
+};
