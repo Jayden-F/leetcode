28bf41dbeac0c5c7bd71e838a363d0dcdceb7bcb
jayden filippi <jaydenjfilippi@gmail.com>
2024-07-09 17:39:06 +1000

    optimise 21.cpp

--- cpp/src/21.cpp
+++ cpp/src/21.cpp
@@ -1,59 +1,43 @@
 
-struct ListNode {
-  int val;
-  ListNode *next;
-  ListNode() : val(0), next(nullptr) {}
-  ListNode(int x) : val(x), next(nullptr) {}
-  ListNode(int x, ListNode *next) : val(x), next(next) {}
+struct ListNode
+{
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) { }
+    ListNode(int x) : val(x), next(nullptr) { }
+    ListNode(int x, ListNode* next) : val(x), next(next) { }
 };
 
-class Solution {
-public:
-  ListNode *mergeTwoLists(ListNode *list1, ListNode *list2) {
+class Solution
+{
+  public:
+    ListNode*
+    mergeTwoLists(ListNode* list1, ListNode* list2)
+    {
 
-    ListNode *head = nullptr;
-    ListNode *tail = nullptr;
-    ListNode *node;
+        ListNode* head = new ListNode();
+        ListNode* tail = head;
 
-    while (list1 != nullptr && list2 != nullptr) {
-      if (list1->val < list2->val) {
-        node = new ListNode(list1->val);
-        list1 = list1->next;
-      } else {
-        node = new ListNode(list2->val);
-        list2 = list2->next;
-      }
+        while(list1 != nullptr && list2 != nullptr)
+        {
+            int min_val;
+            if(list1->val < list2->val)
+            {
+                min_val = list1->val;
+                list1 = list1->next;
+            }
+            else
+            {
+                min_val = list2->val;
+                list2 = list2->next;
+            }
+            tail->next = new ListNode(min_val);
+            tail = tail->next;
+        }
 
-      if (head == nullptr && tail == nullptr) {
-        head = tail = node;
-      } else {
-        tail->next = node;
-        tail = node;
-      }
+        if(list1 != nullptr) { tail->next = list1; }
+        if(list2 != nullptr) { tail->next = list2; }
+
+        return head->next;
     }
-
-    while (list1 != nullptr) {
-      node = new ListNode(list1->val);
-      if (head == nullptr && tail == nullptr) {
-        head = tail = node;
-      } else {
-        tail->next = node;
-        tail = node;
-      }
-        list1 = list1->next;
-    }
-
-    while (list2 != nullptr) {
-      node = new ListNode(list2->val);
-      if (head == nullptr && tail == nullptr) {
-        head = tail = node;
-      } else {
-        tail->next = node;
-        tail = node;
-      }
-        list2 = list2->next;
-    }
-
-    return head;
-  }
 };
